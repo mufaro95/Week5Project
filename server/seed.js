180def46c2b40d701cf2e41c8c5a49dc769d0fba
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-02-05 11:37:37 +0000

    server-client set up

--- server/seed.js
+++ server/seed.js
@@ -1,5 +1,9 @@
+// Setup
+
 import Database from "better-sqlite3";
 const db = new Database("database.db");
+
+// Creating table for database
 
 db.exec(`CREATE TABLE IF NOT EXISTS scoreBoard (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
---
+++ server/server.js
@@ -0,0 +1,43 @@
+// Set up for server side
+
+import express from "express";
+import cors from "cors";
+import Database from "better-sqlite3";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const db = new Database("database.db");
+
+const PORT = "2020";
+app.listen(PORT, () => {
+  console.log(`Server is live on port: ${PORT}`);
+});
+
+// Creating GET and POST so we can test database
+
+// POST
+
+app.post("/scoreBoard", (req, res) => {
+  try {
+    const username = req.body.username;
+    const score = req.body.message;
+    const newScore = db
+      .prepare(`INSERT INTO scoreBoard (username, score) VALUES(?, ?)`)
+      .run(username, score);
+    res.status(200).json({ message: newScore });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
+//GET
+
+app.get("/scoreBoard", (req, res) => {
+  try {
+    let scoreBoard = db.prepare(`SELECT * FROM scoreBoard`).all();
+    res.status(200).json(scoreBoard);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
